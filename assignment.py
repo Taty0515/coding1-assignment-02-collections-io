2058b1a344314ded439a71cf00d1463d297e4412
Tatyana Johnson <tatyaj0515@gmail.com>
2026-09-23 17:12:42 +0200

    ran the script and added comments

--- assignment.py
+++ assignment.py
@@ -2,5 +2,5 @@ drink = "tea"
 sales = {"tea": 3, "coffee": 5}
 sold = sales[drink]
 sold = sold + 1
-print(drink)
-print(sold)
+print(drink) # tea will be printed
+print(sold) # 4 will be printed because sold refers to the sales of the drink and the drink is tea and tea is 3.
